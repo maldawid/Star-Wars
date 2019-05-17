f4db85d7bbe97fcf650c62976d35217ed16551a0
maldawid@gmail.com <maldawid@gmail.com>
2019-05-17 08:49:35 +0200

    Updates

---
+++ precache-manifest.df0412d808dd613494e642d10ff853a2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "79977f053d6f42e5e0575fa2c33b24cd",
+    "url": "/Star-Wars/static/media/LOGO.79977f05.svg"
+  },
+  {
+    "revision": "8bbd21d36c77b175dfaa28e43fcedf7a",
+    "url": "/Star-Wars/static/media/LOADER.8bbd21d3.svg"
+  },
+  {
+    "revision": "da45c4c0de0efd2c6e1fd463b04d3b3f",
+    "url": "/Star-Wars/static/media/ARROW OPEN.da45c4c0.svg"
+  },
+  {
+    "revision": "81e948ad0179ac79be6c28982906dae1",
+    "url": "/Star-Wars/static/media/ARROW CLOSE.81e948ad.svg"
+  },
+  {
+    "revision": "8679479757494aa54f1a",
+    "url": "/Star-Wars/static/js/runtime~main.86794797.js"
+  },
+  {
+    "revision": "0f67aef890860651029d",
+    "url": "/Star-Wars/static/js/main.0f67aef8.chunk.js"
+  },
+  {
+    "revision": "d0219a9e600f6bc71ba6",
+    "url": "/Star-Wars/static/js/2.d0219a9e.chunk.js"
+  },
+  {
+    "revision": "0f67aef890860651029d",
+    "url": "/Star-Wars/static/css/main.b63fca46.chunk.css"
+  },
+  {
+    "revision": "c3b348fdaca829afad33dc7792bf220c",
+    "url": "/Star-Wars/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Star-Warsnpm%20/precache-manifest.45c0f92609b01255884a99333d2fed4a.js"
+  "/Star-Wars/precache-manifest.df0412d808dd613494e642d10ff853a2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Star-Warsnpm%20/index.html", {
+workbox.routing.registerNavigationRoute("/Star-Wars/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
